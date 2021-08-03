1ddfc23383f27c800add7045442def656eb876ab
Neil Ashton <nashton@kevel.co>
2021-08-03 16:00:04 -0400

    Transcribe full example from UpChunk docs (https://github.com/muxinc/upchunk)

--- src/cljs/app/core.cljs
+++ src/cljs/app/core.cljs
@@ -1,25 +1,33 @@
 (ns app.core
   (:require [cljs-http.client :as http]
-            [cljs.core.async :refer [<!]])
+            [cljs.core.async :refer [<!]]
+            ["@mux/upchunk" :as upchunk])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
 (goog-define MUX_TOKEN_ID "")
 (goog-define MUX_TOKEN_SECRET "")
 
-(comment
-;; curl https://api.mux.com/video/v1/uploads \
-;;   -X POST \
-;;   -d '{ "cors_origin": "*", "new_asset_settings": { "playback_policy": ["public"] }'
-;;   -H "Content-Type: application/json" \
-;;   -u ${MUX_TOKEN_ID}:${MUX_TOKEN_SECRET}  
+(def picker (.getElementById js/document "picker"))
 
-  (go
-    (let [response (<! (http/post "https://api.mux.com/video/v1/uploads"
-                                  {:json-params {"cors_origin"        "*"
-                                                 "new_asset_settings" {"playback_policy" ["public"]}}
-                                   :headers     {"Content-Type" "application/json"}
-                                   :basic-auth  {:username MUX_TOKEN_ID
-                                                 :password MUX_TOKEN_SECRET}}))]
-      (prn response)))
-  
-  )
+(defn- upload-url-c
+  []
+  (http/post "https://api.mux.com/video/v1/uploads"
+             {:json-params {"cors_origin"        "*"
+                            "new_asset_settings" {"playback_policy" ["public"]}}
+              :headers     {"Content-Type" "application/json"}
+              :basic-auth  {:username MUX_TOKEN_ID
+                            :password MUX_TOKEN_SECRET}}))
+
+(set!
+ (.-onchange picker)
+ (fn []
+   (go (let [response (<! (upload-url-c))
+             upload-url (get-in response [:body :data :url])]
+         (doto (upchunk/createUpload (clj->js {"endpoint"  upload-url
+                                               "file"      (-> picker
+                                                               .-files
+                                                               (aget 0))
+                                               "chunkSize" 5120}))
+           (.on "error" (fn [err] (.error js/console "Error!" err)))
+           (.on "progress" (fn [progress] (.info js/console "Progress so far: " (.-detail progress) "%")))
+           (.on "success" (fn [_success] (.log js/console "Wrap it up, we're done here. 👋"))))))))
